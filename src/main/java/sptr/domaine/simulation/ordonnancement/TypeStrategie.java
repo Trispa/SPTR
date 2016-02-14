738c49f7132d79e6a66beecc8278906f04d99b16
trispa <trispa88@gmail.com>
2016-02-14 01:07:28 -0500

    refactoring

---
+++ src/main/java/sptr/domaine/simulation/ordonnancement/TypeStrategie.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sptr.domaine.simulation.ordonnancement;
+
+/**
+ *
+ * @author Erco
+ */
+public enum TypeStrategie {
+    ROUND_ROBIN,RATE_MONOTONIC,RATE_MONOTONIC_HERITAGE,EARLIEST_DEDLINE_FIRST,LEAS_SLACK
+}

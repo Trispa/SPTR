5cda445e3f4513a25556983872a51d6536392c98
trispa <trispa88@gmail.com>
2016-03-19 11:49:00 -0400

    implementation du simulateur

---
+++ src/main/java/sptr/domaine/simulation/Default.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sptr.domaine.simulation;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author trispa
+ */
+public class Default implements  Serializable{
+    
+    public static final String VEHICULE_PATH = "/images/auto.png";
+    public static final String FEU_JAUNE_PATH = "/images/feu.png";
+    public static final String FEU_ROUGE_PATH = "/images/feu.png";
+    public static final String FEU_VERT_PATH = "/images/feu.png";
+    public static final String ROUTE_PATH = "/images/route.png";
+    public static final String SAVE_ICON_PATH = "/images/save.png";
+    public static final String UNDO_ICON_PATH = "/images/Undo-icon.png";
+    public static final String REDO_ICON_PATH = "/images/Redo-icon.png";
+    public static final String SETTINGS_ICON_PATH = "/images/settings.png";
+    
+    public static final String PLAYS_IMAGE_PATH = "/images/play.png";
+    public static final String PAUSE_IMAGE_PATH = "/images/pause.png";
+    public static final String RESET_IMAGE_PATH = "/images/reset.png";
+    public static final String TERMINER_IMAGE_PATH = "/images/terminer.png";
+    public static final String URGENCE_IMAGE_PATH = "/images/urgence.png";
+    
+    public static final String GRILLE_ICON_PATH = "/images/grille.png";
+    
+    public static final String ZOOMMOINS = "-";
+    public static final String ZOOMPLUS = "+";
+    
+    public static final int WIDTH_ICON = 40;
+    public static final int HEIGHT_ICON = 40;
+    public static final int BORDER_SIZE = 20;
+    public static final int METRE_PAR_STEP = 10;
+    public static final int CARTE_WIDTH = 900;
+    public static final int CARTE_HEIGHT =640;
+    public static final int INFINI = 999999999;
+    
+    public static final float VITESSE_VEHICULE = 100;
+    public static final int ECHELLE_TEMPS = 3600;
+    public static final float TEMPS_TRAITEMENT = 0.1f;
+    public static final float ZOOM = 1f;
+    
+    
+    public static final String SHOW = "show";
+  
+}
